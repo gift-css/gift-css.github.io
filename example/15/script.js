f1f296dcb516438b12cd243cb4a9deacac78aa6f
coffeemil <ecalm5@gmail.com>
2025-01-17 15:22:07 +0800

    perf: add animationend

--- example/15/script.js
+++ example/15/script.js
@@ -2,3 +2,9 @@ const button = document.querySelector('.button')
 button.addEventListener('click', () => {
   button.classList.add('active')
 })
+
+button.addEventListener('animationend', () => {
+  setTimeout(() => {
+    button.classList.remove('active')
+  }, 3000)
+})
