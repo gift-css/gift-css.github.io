045798b7c235f72fdb923fef476126e3d58a3a15
coffeemil <ecalm5@gmail.com>
2025-01-21 14:57:30 +0800

    perf: update animation delay

--- example/16/script.js
+++ example/16/script.js
@@ -4,11 +4,13 @@ const canceledWords = document.querySelectorAll('.canceled span')
 const canceled = document.querySelector('.canceled')
 
 function init () {
+  let i = 0
   paysWords.forEach((item, index) => {
     item.style.animationDelay = `${index * 0.1}s`
+    i = i + 1
   })
   canceledWords.forEach((item, index) => {
-    item.style.animationDelay = `${index * 0.1}s`
+    item.style.animationDelay = `${index * 0.1 + i * 0.1}s`
   })
 }
 init()
