869a7d0f476c34fce88124c13f4ba8003d289e06
coffeemil <ecalm5@gmail.com>
2025-01-23 12:15:52 +0800

    revert：update License

--- example/config.js
+++ example/config.js
@@ -1,10 +1,8 @@
 // 文件夹名称：项目名称
 export const exampleConfig = {
   '1': "脉冲波纹催眠动画特效",
-  '2': "3D导航栏",
   '3': "头像轮播",
   '4': "animated-countdown",
-  '5': "Auto Text Effect",
   '6': "常规loading",
   '7': "抛出线条式loading",
   '8': "长方形进度条",
