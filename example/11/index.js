8b6f280ec5fa4b816f7356dfad94c3bf15caef68
coffeemil <ecalm5@gmail.com>
2025-03-10 10:33:21 +0800

    revert: optimize example 11th's css

--- example/11/index.js
+++ example/11/index.js
@@ -1,9 +1,19 @@
+/*
+ * @Author: coffeemil ecalm5@gmail.com
+ * @Date: 2025-02-27 10:45:37
+ * @LastEditors: coffeemil ecalm5@gmail.com
+ * @LastEditTime: 2025-03-10 09:55:40
+ * @FilePath: \gift-css\example\11\index.js
+ */
 const button = document.querySelector('.button')
 button.addEventListener('click', () => {
-  console.log()
   if (Array.from(button.classList).includes('active')) {
     button.classList.remove('active')
+    button.classList.add('inactive')
   } else {
+    button.classList.remove('inactive')
     button.classList.add('active')
   }
 })
+
+       
