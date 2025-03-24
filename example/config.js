884d5af132e79989d76d55f245aa6acb129d654a
coffeemil <ecalm5@gmail.com>
2025-03-24 15:15:52 +0800

    chore: reorganize and clean up example files for better maintainability
    Moved various example files to the ignore directory and updated the config.js to reflect the changes. This reorganization helps in maintaining a cleaner and more structured project directory.

--- example/config.js
+++ example/config.js
@@ -2,22 +2,22 @@
  * @Author: coffeemil ecalm5@gmail.com
  * @Date: 2025-02-10 16:58:04
  * @LastEditors: coffeemil ecalm5@gmail.com
- * @LastEditTime: 2025-03-17 14:07:24
+ * @LastEditTime: 2025-03-24 15:06:54
  * @FilePath: \gift-css\example\config.js
  * @Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
  */
 // 文件夹名称：项目名称
 export const exampleConfig = {
-  '1': "Concentric Pulse Layers", // 多层同心脉冲扩散
+  // '1': "Concentric Pulse Layers", // 多层同心脉冲扩散
   '2': "Animated Submit-to-Success Button Transition",  // 灵动提交成功按钮动画
-  '3': "Photo Carousel", // 照片轮播
-  '4': "CountdownTransitionPanel", // 倒计时GO按钮动画
+  // '3': "Photo Carousel", // 照片轮播
+  // '4': "CountdownTransitionPanel", // 倒计时GO按钮动画
   '5': "Width-Shift Animated Button", // 宽变灵动按钮
-  '6': "常规loading",
-  '7': "抛出线条式loading",
-  '8': "长方形进度条",
-  '9': "椭圆式进度条loading",
-  '10': "3D Flip Card Effect", // 3D 卡片翻转特效
+  // '6': "常规loading",
+  // '7': "抛出线条式loading",
+  // '8': "长方形进度条",
+  // '9': "椭圆式进度条loading",
+  // '10': "3D Flip Card Effect", // 3D 卡片翻转特效
   '11': "Switch Transition Button", // 开关过渡按钮
   '12': "Rounded Rectangular Button with Line Animation", // 带线条动画的圆角长方形按钮
   '13': "You Tube Video Play Button Animation", // YouTube 视频播放按钮动画
