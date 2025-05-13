6ca13464107b63e011325821b7415da69eb449d2
coffeemil <ecalm5@gmail.com>
2025-05-13 17:39:06 +0800

    refactor(button): simplify event listeners and enhance animation
    Remove redundant mouse event listeners and replace with a single click event. Add new SVG elements and CSS animations to improve the visual feedback on button activation. This change improves maintainability and enhances user experience.

--- example/15/index.js
+++ example/15/index.js
@@ -1,13 +1,9 @@
+/*
+ * @Date: 2025-04-22 17:40:41
+ * @LastEditors: coffeemil ecalm5@gmail.com
+ * @LastEditTime: 2025-05-13 17:36:01
+ */
 const button = document.querySelector('.button')
-button.addEventListener('mousedown', () => {
+button.addEventListener('click', () => {
   button.classList.add('active')
-})
-
-button.addEventListener('mouseup', () => {
-  button.classList.remove('active')
-})
-
-// 添加鼠标离开事件监听
-button.addEventListener('mouseleave', () => {
-  button.classList.remove('active')
 })
